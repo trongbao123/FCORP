94f1db89a8053bbddd370111a0c8c328dcca2288
unknown <phambao11062001@gmail.com>
2023-07-15 09:10:53 +0700

    up load source

---
+++ my-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+import { RouterView } from "vue-router"
+export default {
+  name: "App",
+  components: { RouterView }
+}
+</script>
+
+<style>
+.container {
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
